570e92ffed7762ccb7904172f3732df08739911d
Sophie Johnson <sophjohnson12@gmail.com>
2026-04-11 09:50:05 -0400

    Adjust info modal margins

--- src/components/game/InfoModal.tsx
+++ src/components/game/InfoModal.tsx
@@ -20,15 +20,15 @@ interface InfoModalProps {
 export default function InfoModal({ minSongLyricCount, minImageCount, maxImageCount, guessCount, songCount, albums, showAlbumFilters, showFlagIcon, showMapButton, levelName, onClose }: InfoModalProps) {
   return (
     <Modal onClose={onClose} showEaseIn={true}>
-      <h2 className="text-xl font-bold text-primary mb-1 tracking-wide">How to Play</h2>
-      <div className="text-sm mb-1">
+      <h2 className="text-xl font-bold text-primary md:mb-1 tracking-wide">How to Play</h2>
+      <div className="text-sm mb-0.5 md:mb-1">
         <span className="text-neutral-600 font-semibold">Level: </span>
         <span className="text-neutral-700 font-normal">{levelName}</span>
       </div>
-      <div className="space-y-2 md:space-y-3 text-base text-neutral-600">
+      <div className="space-y-1 md:space-y-2 text-base text-neutral-600">
         <div>
-          <h3 className="font-semibold text-neutral-800 tracking-wide mb-1">1. Guess the {minSongLyricCount === 1 ? 'Word' : `${minSongLyricCount > 0 ? `${minSongLyricCount} ` : ''}Words`}</h3>
-          <p className="mb-1">
+          <h3 className="font-semibold text-neutral-800 tracking-wide mb-0.5 md:mb-1">1. Guess the {minSongLyricCount === 1 ? 'Word' : `${minSongLyricCount > 0 ? `${minSongLyricCount} ` : ''}Words`}</h3>
+          <p className="mb-0.5">
             <span className="inline md:hidden">
               {minSongLyricCount === 1 ? 'The tab' : 'Each tab'} has {minImageCount}–{maxImageCount} pictures that represent a word from the song.
               Swipe to see all pictures. 
@@ -46,7 +46,7 @@ export default function InfoModal({ minSongLyricCount, minImageCount, maxImageCo
         </div>
         {showAlbumFilters && (
           <div>
-            <h3 className="tracking-wide font-semibold text-neutral-800 mb-1">2. Guess the Album</h3>
+            <h3 className="tracking-wide font-semibold text-neutral-800 mb-0.5 md:mb-1">2. Guess the Album</h3>
             <div>
               {albums.length > 0 && <AlbumButtons albums={albums} readonly list />}
             </div>
@@ -54,7 +54,7 @@ export default function InfoModal({ minSongLyricCount, minImageCount, maxImageCo
         )}
         {!showAlbumFilters && (
           <div>
-            <h3 className="tracking-wide font-semibold text-neutral-800 mb-1">2. Show the Album</h3>
+            <h3 className="tracking-wide font-semibold text-neutral-800 mb-0.5 md:mb-1">2. Show the Album</h3>
             <p>
               <span className="hidden md:inline">Click </span>
               <span className="inline md:hidden">Tap </span>
@@ -63,11 +63,11 @@ export default function InfoModal({ minSongLyricCount, minImageCount, maxImageCo
           </div>
         )}
         <div>
-          <h3 className="font-semibold text-neutral-800 mb-1">3. Guess the Song</h3>
-          <p className="mb-1">
+          <h3 className="font-semibold text-neutral-800 mb-0.5 md:mb-1">3. Guess the Song</h3>
+          <p className="mb-0.5 md:mb-1">
             You have {guessCount} chances. Play all {songCount} songs!
           </p>
-          <ul>
+          <ul className="space-y-0.5 md:space-y-1">
             <li className="flex items-center"><Sliders size={15} strokeWidth={3} className="mr-2 text-primary"/>Manage levels and game history</li>
             {showMapButton && <li className="flex items-center"><Map size={15} strokeWidth={3} className="mr-2 text-primary"/>View landmark map</li>}
             <li className="flex items-center"><SkipForward size={15} strokeWidth={3} className="mr-2 text-primary"/>Skip song</li>
